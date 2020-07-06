249d6e26399d66717a3751fc9f06be04080dda74
Emerson Reis <Emerson.Lopes.Reis@formandos.cinel.pt>
2020-07-06 18:53:32 +0100

    Add ImageHelper and ConverterHelper

--- ShopCet46.Web/Controllers/ProductsController.cs
+++ ShopCet46.Web/Controllers/ProductsController.cs
@@ -1,28 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShopCet46.Web.Data;
-using ShopCet46.Web.Data.Entities;
 using ShopCet46.Web.Helpers;
 using ShopCet46.Web.Models;
-using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ShopCET46.Web.Controllers
 {
+    
     public class ProductsController : Controller
     {
 
         private readonly IProductRepository _productRepository;
         private readonly IUserHelper _userHelper;
+        private readonly IImageHelper _imageHelper;
+        private readonly IConverterHelper _converterHelper;
 
-
-        public ProductsController(IProductRepository productRepository, IUserHelper userHelper)
+        public ProductsController(
+            IProductRepository productRepository,
+            IUserHelper userHelper,
+            IImageHelper imageHelper,
+            IConverterHelper converterHelper)
         {
 
             _productRepository = productRepository;
             _userHelper = userHelper;
+            _imageHelper = imageHelper;
+            _converterHelper = converterHelper;
         }
 
 
@@ -50,6 +56,7 @@ namespace ShopCET46.Web.Controllers
             return View(product);
         }
 
+        [Authorize]
         // GET: Products/Create
         public IActionResult Create()
         {
@@ -66,29 +73,15 @@ namespace ShopCET46.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                
+
                 var path = string.Empty;
 
                 if (model.ImageFile != null && model.ImageFile.Length > 0)
                 {
-                    var guid = Guid.NewGuid().ToString();
-
-                    var file = $"{guid}.jpg";
-
-                    path = Path.Combine(
-                        Directory.GetCurrentDirectory(),
-                        "wwwroot\\images\\Products",
-                        file);
-
-                    using (var stream = new FileStream(path, FileMode.Create))
-                    {
-                        await model.ImageFile.CopyToAsync(stream);
-                    }
-
-                    path = $"~/images/Products/{file}";
+                    path = await _imageHelper.UploadImageAsync(model.ImageFile, "Products");
                 }
 
-                var product = this.ToProduct(model, path);
+                var product = _converterHelper.ToProduct(model, path, true);
 
                 //TODO: Change to the logged user
                 product.User = await _userHelper.GetUserByEmailAsync("rafaasfs@gmail.com");
@@ -98,7 +91,7 @@ namespace ShopCET46.Web.Controllers
             return View(model);
         }
 
-        private Product ToProduct(ProductViewModel view, string path)
+        /*private Product ToProduct(ProductViewModel view, string path)
         {
             return new Product
             {
@@ -112,9 +105,11 @@ namespace ShopCET46.Web.Controllers
                 Stock = view.Stock,
                 User = view.User
             };
-        }
+        }*/
+
 
         // GET: Products/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -129,12 +124,12 @@ namespace ShopCET46.Web.Controllers
             }
 
 
-            var view = this.ToProductViewModel(product);
+            var view = _converterHelper.ToProductViewModel(product);
 
             return View(view);
         }
 
-        private ProductViewModel ToProductViewModel(Product product)
+        /*private ProductViewModel ToProductViewModel(Product product)
         {
             return new ProductViewModel
             {
@@ -148,7 +143,7 @@ namespace ShopCET46.Web.Controllers
                 Stock = product.Stock,
                 User = product.User
             };
-        }
+        }*/
 
 
 
@@ -168,24 +163,10 @@ namespace ShopCET46.Web.Controllers
 
                     if (model.ImageFile != null && model.ImageFile.Length > 0)
                     {
-                        var guid = Guid.NewGuid().ToString();
-
-                        var file = $"{guid}.jpg";
-
-                        path = Path.Combine(
-                            Directory.GetCurrentDirectory(),
-                            "wwwroot\\images\\Products",
-                            file);
-
-                        using (var stream = new FileStream(path, FileMode.Create))
-                        {
-                            await model.ImageFile.CopyToAsync(stream);
-                        }
-
-                        path = $"~/images/Products/{file}";
+                        path = await _imageHelper.UploadImageAsync(model.ImageFile, "Products");
                     }
 
-                    var product = this.ToProduct(model, path);
+                    var product = _converterHelper.ToProduct(model, path, false);
 
                     //TODO: Change to the logged user
                     product.User = await _userHelper.GetUserByEmailAsync("rafaasfs@gmail.com");
@@ -208,6 +189,7 @@ namespace ShopCET46.Web.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
---
+++ ShopCet46.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,16 @@
+﻿using ShopCet46.Web.Data.Entities;
+using ShopCet46.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCet46.Web.Helpers
+{
+    public interface IConverterHelper
+    {
+        Product ToProduct(ProductViewModel model, string path, bool isNew);
+
+        ProductViewModel ToProductViewModel(Product model);
+    }
+}
